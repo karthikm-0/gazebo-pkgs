2ebf1be0223471a4fe9e23bb4a38309481f351c8
Karthik <karthikm@dgp.toronto.edu>
2020-01-10 08:18:52 -0800

    Can spawn and recognize cubes so their TF is published

--- gazebo_test_tools/src/spawn_cubes_node.cpp
+++ gazebo_test_tools/src/spawn_cubes_node.cpp
@@ -1,9 +1,12 @@
 #include <gazebo_test_tools/gazebo_cube_spawner.h>
+#include <gazebo_test_tools/FakeObjectRecognizer.h>
 #include </home/karthikm/Desktop/json.hpp>
 
 #include <iostream>
 #include <sstream>
 #include <fstream>
+
+#define DEFAULT_RECOGNIZE_OBJECT_TOPIC "/gazebo/recognize_object"
 
 using json = nlohmann::json;
 
@@ -15,9 +18,21 @@ int main(int argc, char** argv) {
 
     ROS_INFO("Running spawn cube once..");
 
+    // Spawn cubes frame
     std::string frame_id="world";
 
-    struct cube_spawn_parameters {
+    // Recognizing cubes topic
+	std::string RECOGNIZE_OBJECT_TOPIC;
+
+    node.param<std::string>("gazebo_test_tools/recognize_object_service", RECOGNIZE_OBJECT_TOPIC, DEFAULT_RECOGNIZE_OBJECT_TOPIC);
+	ROS_INFO("Got recognize object service topic name: <%s>", RECOGNIZE_OBJECT_TOPIC.c_str());
+
+    // Create service for recognizing the cubes that spawn
+    ros::ServiceClient client = node.serviceClient<gazebo_test_tools::RecognizeGazeboObject>(RECOGNIZE_OBJECT_TOPIC);
+    gazebo_test_tools::RecognizeGazeboObject srv;
+
+    struct cube_spawn_parameters 
+    {
         std::string name;
         float x;
         float y;
@@ -26,6 +41,7 @@ int main(int argc, char** argv) {
         float mass=0.05;
     } ;
 
+    // Spawning cubes from file
     std::ifstream file("/home/karthikm/Desktop/cube_spawn_info.txt");
     std::string str; 
     cube_spawn_parameters * sp = new cube_spawn_parameters;
@@ -48,9 +64,27 @@ int main(int argc, char** argv) {
         sp->x = stof(params[1]);
         sp->y = stof(params[2]);
         sp->z = stof(params[3]); 
+
+        // Spawn
+
         spawner.spawnCube(sp->name, frame_id, sp->x, sp->y, sp->z, 0, 0, 0, 1,
         sp->dim, sp->dim, sp->dim, sp->mass);
+        
+        // Recognize
+        srv.request.name = sp->name;
+        srv.request.republish = 1;  // Keep publishing the states
+    
+        if (client.call(srv))
+        {
+            ROS_INFO("Result:");
+            std::cout<<srv.response<<std::endl;
+        }
+        else
+        {
+            ROS_ERROR("Failed to call service %s",RECOGNIZE_OBJECT_TOPIC.c_str());
+            return 1;
+        }
     }
-
+    
     return 0;
 }
